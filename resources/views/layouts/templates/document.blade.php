81d119f66367a4fa7e493e29346eddf96be1f521
qfkong <kong.qf@mdg-app.com>
2018-02-27 13:45:27 +0800

    1、细调界面样式

--- resources/views/layouts/templates/document.blade.php
+++ resources/views/layouts/templates/document.blade.php
@@ -1,47 +1,7 @@
 <template>
 	<layout>
 		<sider hide-trigger>
-			<i-menu active-name="1-2" accordion="true" theme="light" width="auto" :open-names="['1']" :style="{height:layoutContentHeight-38-11-2-11+'px'}">
-				<submenu name="1">
-					<template slot="title">
-						<icon type="ios-navigate"></icon>
-						Item 1
-					</template>
-					<menu-item name="1-1">
-						Option 1
-					</menu-item>
-					<menu-item name="1-2">
-						Option 2
-					</menu-item>
-					<menu-item name="1-3">
-						Option 3
-					</menu-item>
-				</submenu>
-				<submenu name="2">
-					<template slot="title">
-						<icon type="ios-keypad"></icon>
-						Item 2
-					</template>
-					<menu-item name="2-1">
-						Option 1
-					</menu-item>
-					<menu-item name="2-2">
-						Option 2
-					</menu-item>
-				</submenu>
-				<submenu name="3">
-					<template slot="title">
-						<icon type="ios-analytics"></icon>
-						Item 3
-					</template>
-					<menu-item name="3-1">
-						Option 1
-					</menu-item>
-					<menu-item name="3-2">
-						Option 2
-					</menu-item>
-				</submenu>
-			</i-menu>
+			<tree :data="item.tree" :style="{height:layoutContentHeight-38-11-2-11+'px'}"></tree>
 		</sider>
 		<layout>
 			<breadcrumb :style="{margin: '24px 0'}">
--- resources/views/layouts/templates/nav-menu.blade.php
+++ resources/views/layouts/templates/nav-menu.blade.php
@@ -2,23 +2,15 @@
 	<row>
 		<i-col span="8">
 			<i-menu theme="light" active-name="1-1" open-names="[1]" on-select="menuActive" :style="{height:layoutContentHeight+'px',width: '200px'}">
-				<submenu name="1">
-					<template slot="title">
-						<icon type="ios-paper"></icon>
-						知识管理
-					</template>
+				<menu-group title="知识管理">
 					<menu-item name="1-1">
 						知识查询
 					</menu-item>
 					<menu-item name="1-2">
 						知识维护
 					</menu-item>
-				</submenu>
-				<submenu name="2">
-					<template slot="title">
-						<icon type="ios-people"></icon>
-						考试管理
-					</template>
+				</menu-group>
+				<menu-group title="考试管理">
 					<menu-item name="2-1">
 						题库管理
 					</menu-item>
@@ -31,19 +23,15 @@
 					<menu-item name="2-4">
 						我的考试
 					</menu-item>
-				</submenu>
-				<submenu name="3">
-					<template slot="title">
-						<icon type="stats-bars"></icon>
-						系统管理
-					</template>
+				</menu-group>
+				 <menu-group title="系统管理">
 					<menu-item name="3-1">
 						单位管理
 					</menu-item>
 					<menu-item name="3-2">
 						用户管理
 					</menu-item>
-				</submenu>
+				</menu-group>
 			</i-menu>
 		</i-col>
 	</row>
